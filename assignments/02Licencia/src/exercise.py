10889c5c2b075807e7d0d2e43ba523e8f817abc5
Luis Mario Hernandez <A01275683@itesm.mx>
2021-08-30 00:49:50 -0500

    Condicionales basico

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -3,5 +3,21 @@ def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
 
+    if edad >= 18:
+
+        identificacion = (input("¿Tienes identificación oficial? (s/n): "))
+        if identificacion == "s" :
+            print("Trámite de licencia concedido")
+        elif identificacion == "n" :
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+            
+    elif edad < 18 and edad > 0:
+            print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
+        
+
 if __name__ == '__main__':
     main()
